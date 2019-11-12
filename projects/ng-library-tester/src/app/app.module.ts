47726d9d4c7fd16aaabc19fe48081b78efb06a61
aakash.t <aakash.t@BMMDVAAKASHT.local>
2019-11-12 23:16:02 +0530

    trying to add husky

--- projects/ng-library-tester/src/app/app.module.ts
+++ projects/ng-library-tester/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgLibraryToddModule } from "ng-library-todd";
+import { NgLibraryToddModule } from 'ng-library-todd';
 
 @NgModule({
   declarations: [
